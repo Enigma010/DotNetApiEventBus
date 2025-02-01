4672c15f4748984bda920fe004ebf16b850d1c63
Enigma010 <jjodarsk@yahoo.com>
2025-01-31 20:21:51 -0600

    Initial commit

---
+++ EventAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetApiEventBus
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    [ExcludeFromCodeCoverage(Justification = "Core infrastructure, unit tests would at a lower level")]
+    public class EventAttribute : Attribute
+    {
+        public EventAttribute() : base() { }
+    }
+}
